950f9fee79fb1d6a8395b528f2990dda6f122f1c
Kurt Miller <bsdkurt@gmail.com>
2026-07-21 21:57:34 -0400

    Fix os::naked_short_nanosleep on OpenBSD when < 20 miliseconds. nanosleep(2) on OpenBSD is currently accurate down to 20 miliseconds. Below 20 miliseconds, use a busy loop with SpinPause. This fixes serviceability/jvmti/vthread/ThreadStateTest/ThreadStateTest failure on OpenBSD.

--- src/hotspot/os/posix/os_posix.cpp
+++ src/hotspot/os/posix/os_posix.cpp
@@ -1004,6 +1004,7 @@ void os::infinite_sleep() {
   }
 }
 
+#ifndef __OpenBSD__
 void os::naked_short_nanosleep(jlong ns) {
   struct timespec req;
   assert(ns > -1 && ns < NANOUNITS, "Un-interruptable sleep, short time use only");
@@ -1012,6 +1013,7 @@ void os::naked_short_nanosleep(jlong ns) {
   ::nanosleep(&req, nullptr);
   return;
 }
+#endif
 
 void os::naked_short_sleep(jlong ms) {
   assert(ms < MILLIUNITS, "Un-interruptable sleep, short time use only");
